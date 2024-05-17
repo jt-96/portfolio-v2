0bffb3e76214b26ae137160ad2f24c61e6309171
jt-96 <jetorrico@outlook.com>
2024-05-17 16:34:09 -0300

    feature: add acui moving cards, add media queries

---
+++ src/components/movingCards/cards.component.tsx
@@ -0,0 +1,116 @@
+"use client";
+import { cn } from "@/utils/cn";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+
+export const InfiniteMovingCards = ({
+  items,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}: {
+  items: {
+    icon: any;
+    name: string;
+  }[];
+  direction?: "left" | "right";
+  speed?: "fast" | "normal" | "slow";
+  pauseOnHover?: boolean;
+  className?: string;
+}) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "60s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "200s");
+      }
+    }
+  };
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20 max-w-xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_50%,white_50%,transparent)] md:max-w-3xl lg:max-w-4xl xl:max-w-7xl 2xl:[mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)] 2xl:max-w-[120rem]",
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          "flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
+          start && "animate-scroll",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+        {items.map((item, idx) => (
+          <li
+            className="w-[160px] h-[100px] relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 flex flex-col justify-center"
+            style={{
+              background:
+                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+            }}
+            key={item.name}
+          >
+            <blockquote className="flex flex-col items-center">
+              <Image
+                src={item.icon}
+                alt="Tech Icon"
+                width={64}
+                height={64}
+                color="#000000"
+                className="p-4"
+              />
+              <div className="z-20 w-full flex">
+                <span className="text-md leading-[1.6] text-gray-400 w-full text-center">
+                  {item.name}
+                </span>
+              </div>
+            </blockquote>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
