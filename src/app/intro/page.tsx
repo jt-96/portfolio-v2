04042259af67a3eb2edd9720f7149f3d59fed649
jt-96 <jetorrico@outlook.com>
2024-05-23 14:42:27 -0300

    feature: add animation to intro page

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 function Intro() {
@@ -32,12 +33,32 @@ function Intro() {
   return (
     <div className="h-screen flex flex-col justify-center">
       <div className="mb-96 2xl:mb-[35rem]">
-        <h1 className="text-2xl text-center md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-7xl">
-          Hi, I'm Jonathan Torrico
-        </h1>
-        <p className="text-lg text-center md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-6xl">
-          {currentTitle}
-        </p>
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ ease: "easeInOut", delay: 0.3, duration: 0.4 }}
+        >
+          <h1 className="text-2xl text-center md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-7xl">
+            Hi, I'm Jonathan Torrico
+          </h1>
+        </motion.div>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={currentTitle}
+            initial={{ opacity: 0.0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            exit={{
+              opacity: 0.0,
+              y: -40,
+              transition: { delay: 0.0, duration: 0.3 },
+            }}
+            transition={{ ease: "easeInOut", delay: 0.3, duration: 0.4 }}
+          >
+            <p className="text-lg text-center md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-6xl">
+              {currentTitle}
+            </p>
+          </motion.div>
+        </AnimatePresence>
       </div>
     </div>
   );
