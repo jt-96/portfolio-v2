2b0145aa1550d31715d4c7412f94774a70d3e909
jt-96 <jetorrico@outlook.com>
2024-05-23 14:45:32 -0300

    feature: add transition to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { InfiniteMovingCards } from "@/components/movingCards/cards.component";
 import { frontStack } from "@/models/front";
 import { backStack } from "@/models/back";
@@ -6,10 +7,27 @@ import { others } from "@/models/other";
 function Profile() {
   return (
     <>
-      <h2 className="text-3xl my-5">Profile</h2>
-      <InfiniteMovingCards items={frontStack} direction="left" speed="normal" />
-      <InfiniteMovingCards items={backStack} direction="right" speed="normal" />
-      <InfiniteMovingCards items={others} direction="left" speed="normal" />
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.4,
+          ease: "easeInOut",
+        }}
+      >
+        <h2 className="text-3xl my-5">Profile</h2>
+        <InfiniteMovingCards
+          items={frontStack}
+          direction="left"
+          speed="normal"
+        />
+        <InfiniteMovingCards
+          items={backStack}
+          direction="right"
+          speed="normal"
+        />
+        <InfiniteMovingCards items={others} direction="left" speed="normal" />
+      </motion.div>
     </>
   );
 }
