c5d5efb1f2df71cbee5059eaf618573276f8426a
jt-96 <jetorrico@outlook.com>
2024-05-23 14:46:28 -0300

    feature: add transition to exp page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,33 +1,47 @@
+import { motion } from "framer-motion";
+
 function Experience() {
   return (
     <>
-      <h1 className="text-3xl my-10 text-center">Experience</h1>
-      <div className="bg-gradient-to-r from-slate-800 to-slate-900 rounded-2xl border border-b-0 border-slate-700 mx-5 p-2 md:m-10 2xl:px-8 2xl:py-4 2xl:my-5">
-        <div className="flex flex-col justify-between align-center items-center my-2 md:flex-row md:mx-5">
-          <h1 className="text-3xl">Vivvidero</h1>
-          <h2 className="text-lg lg:text-2xl">Prop Tech StartUp - Colombia</h2>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.4,
+          ease: "easeInOut",
+        }}
+      >
+        <h1 className="text-3xl my-10 text-center">Experience</h1>
+        <div className="bg-gradient-to-r from-slate-800 to-slate-900 rounded-2xl border border-b-0 border-slate-700 mx-5 p-2 md:m-10 2xl:px-8 2xl:py-4 2xl:my-5">
+          <div className="flex flex-col justify-between align-center items-center my-2 md:flex-row md:mx-5">
+            <h1 className="text-3xl">Vivvidero</h1>
+            <h2 className="text-lg lg:text-2xl">
+              Prop Tech StartUp - Colombia
+            </h2>
+          </div>
+          <div className="flex flex-row mx-5 justify-between align-center items-center md:flex-row md:mx-5">
+            <h3 className="text-md lg:text-2xl">Full Stack Developer</h3>
+            <h3 className="text-md lg:text-2xl">May 2022 - July 2023</h3>
+          </div>
+          <div className="flex flex-col justify-center items-center align-center my-3 mx-5">
+            <ul className="text-xl marker:text-slate-500">
+              <li className="list-disc my-2">
+                Development of Cotibot, a quoutation engine web app for home
+                renovations.
+              </li>
+              <li className="list-disc my-2">
+                Development of several intracompany tools for process
+                optimizations (data exporting, scrappers, renovation
+                management).
+              </li>
+              <li className="list-disc my-2">
+                Assigned with administrative tasks for project management,
+                reporting tickets status, supporting other members of tech team.
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="flex flex-row mx-5 justify-between align-center items-center md:flex-row md:mx-5">
-          <h3 className="text-md lg:text-2xl">Full Stack Developer</h3>
-          <h3 className="text-md lg:text-2xl">May 2022 - July 2023</h3>
-        </div>
-        <div className="flex flex-col justify-center items-center align-center my-3 mx-5">
-          <ul className="text-xl marker:text-slate-500">
-            <li className="list-disc my-2">
-              Development of Cotibot, a quoutation engine web app for home
-              renovations.
-            </li>
-            <li className="list-disc my-2">
-              Development of several intracompany tools for process
-              optimizations (data exporting, scrappers, renovation management).
-            </li>
-            <li className="list-disc my-2">
-              Assigned with administrative tasks for project management,
-              reporting tickets status, supporting other members of tech team.
-            </li>
-          </ul>
-        </div>
-      </div>
+      </motion.div>
     </>
   );
 }
