158779e9e0462d2ea7c763a1fb65148ca572b5b8
jt-96 <jetorrico@outlook.com>
2024-08-06 17:19:46 -0300

    feature: add new project, edit ids for all projects

--- src/models/projects.ts
+++ src/models/projects.ts
@@ -3,8 +3,18 @@ import { Project } from "./project.model";
 export const projects: Project[] = [
   {
     id: 0,
-    imageSrc: "/portfolio-v2/km12.png",
-    title: "Kilometro 12 - Market",
+    imageSrc: "/portfolio-v2/toDoBubble.png",
+    title: "To-Do App - Bubble",
+    description:
+      "Never seen before app that allows you to manage your tasks of the day, this one is made in Bubble, a no-code tool, it also has integration with Xano, a no-code Backend & BBDD, with endpoints for creating, editing, deleting tasks, and client authentication/authorization with JWT.",
+    siteLink: "https://jetorrico.bubbleapps.io/version-test/login",
+    repoLink: null,
+    year: 2024,
+  },
+  {
+    id: 1,
+    imageSrc: "/porfolio-v2/km12.png",
+    title: "Kilometro 12 Market",
     description:
       "E-commerce for fruits and vegetables, including account creation, cart functionality, multiple filtering for available products, several payment methods, like Mercado Pago, admin control for orders, etc.",
     siteLink: "https://km12-develop.netlify.app/",
@@ -12,7 +22,7 @@ export const projects: Project[] = [
     year: 2024,
   },
   {
-    id: 1,
+    id: 2,
     imageSrc: "/portfolio-v2/m-bot.png",
     title: "M-Bot",
     description:
@@ -22,9 +32,9 @@ export const projects: Project[] = [
     year: 2023,
   },
   {
-    id: 2,
+    id: 3,
     imageSrc: "/portfolio-v2/toDoListApp.png",
-    title: "ToDo List App",
+    title: "To-Do List App",
     description:
       "Web app for a ToDo List, allowing you to add and delete chores, saving them in an instance of MongoDB Cloud.",
     siteLink: "https://todolistapp-oz6u.onrender.com/",
@@ -32,7 +42,7 @@ export const projects: Project[] = [
     year: 2022,
   },
   {
-    id: 3,
+    id: 4,
     imageSrc: "/portfolio-v2/keeperApp.png",
     title: "Keeper App",
     description:
@@ -42,7 +52,7 @@ export const projects: Project[] = [
     year: 2022,
   },
   {
-    id: 4,
+    id: 5,
     imageSrc: "/portfolio-v2/digitalAgency.png",
     title: "Digital Agency",
     description: "Mockup page about a digital agency made in Bootstrap.",
