b33debccd9259eeb4b7871bcd7c64c98e23604bd
jt-96 <jetorrico@outlook.com>
2026-04-28 17:00:12 -0300

    fix: general spacing between entries

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -11,7 +11,7 @@ function Experience() {
         transition={variants.showPara.transition}
       >
         <h1 className="text-3xl my-10 text-center">Experience</h1>
-        <div className="bg-gradient-to-r from-slate-800 to-slate-900 rounded-2xl border border-b-0 border-slate-700 mx-5 p-2 md:m-10 2xl:px-8 2xl:py-4 2xl:my-5">
+        <div className="bg-gradient-to-r from-slate-800 to-slate-900 rounded-2xl border border-b-0 border-slate-700 mx-5 px-8 py-4 sm:m-10 md:m-10 2xl:my-5">
           <div className="flex flex-col justify-between align-center items-center my-2 md:flex-row md:mx-5">
             <h1 className="text-3xl">WAYPOINT</h1>
             <h2 className="text-lg lg:text-2xl">
@@ -23,7 +23,7 @@ function Experience() {
             <h3 className="text-md lg:text-2xl">September 2024 - February 2025</h3>
           </div>
           <div className="flex flex-col justify-center items-center align-center my-3 mx-5">
-            <ul className="text-xl marker:text-slate-500">
+            <ul className="text-xl marker:text-slate-500 sm:text-lg">
               <li className="list-disc my-2">
                 Worked alongside CEO and Production Assistant to update the homepage to Webflow.
               </li>
@@ -42,7 +42,7 @@ function Experience() {
         whileInView={variants.onView}
         transition={variants.showPara.transition}
       >
-        <div className="bg-gradient-to-r from-slate-800 to-slate-900 rounded-2xl border border-b-0 border-slate-700 mx-5 p-2 md:m-10 2xl:px-8 2xl:py-4 2xl:my-5">
+        <div className="bg-gradient-to-r from-slate-800 to-slate-900 rounded-2xl border border-b-0 border-slate-700 mx-5 px-8 py-4 sm:m-10 md:m-10 2xl:my-5">
           <div className="flex flex-col justify-between align-center items-center my-2 md:flex-row md:mx-5">
             <h1 className="text-3xl">Vivvidero</h1>
             <h2 className="text-lg lg:text-2xl">
@@ -54,7 +54,7 @@ function Experience() {
             <h3 className="text-md lg:text-2xl">May 2022 - July 2023</h3>
           </div>
           <div className="flex flex-col justify-center items-center align-center my-3 mx-5">
-            <ul className="text-xl marker:text-slate-500">
+            <ul className="text-xl marker:text-slate-500 sm:text-lg">
               <li className="list-disc my-2">
                 Development of Cotibot, a quotation engine web app for home
                 renovations.
